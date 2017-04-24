ae8597f75675894520140be3caf1ae34419f8b31
Etna <etna@xavier-laptop>
2017-04-24 20:11:08 +0200

    xfa: test doctrnine

--- TDLF/back/appli/index.php
+++ TDLF/back/appli/index.php
@@ -1,7 +1,42 @@
 <?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+require_once __DIR__.'/vendor/autoload.php';
 
-require_once __DIR__.'/app/app.php';
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+require_once 'src/User.php';
 
+$isDevMode = false;
+
+$app = new Silex\Application();
+
+$conn = array(
+    'dbname' => 'tdlf_bdd',
+    'user' => 'bx',
+    'password' => 'toto',
+    'host' => 'db',
+    'driver' => 'pdo_mysql',
+);
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode);
+$entityManager = EntityManager::create($conn, $config);
+
+
+$app->get('/blog/{id}', function ($id) use ($entityManager) {
+    $user = new User();
+    $user->setName($id);
+    $entityManager->persist($user);
+    $entityManager->flush();
+    return $user->getId();
+});
+
+$app->get("/login/{id}", function($id) use ($entityManager)) {
+    $entityManager->findAll();
+}
+
+
+$app['debug'] = true;
+
+//Request::setTrustedProxies(array($ip));
 $app->run();
-
 ?>
